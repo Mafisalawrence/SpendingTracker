6d4d10d3e3f31dd14e0564379a402fd7df0be22f
lawrence <lmafisa@retrorabbit.co.za>
2019-03-27 16:08:09 +0200

    added fragments to main activity

--- app/src/main/java/com/example/spendingtracker/MainActivity.kt
+++ app/src/main/java/com/example/spendingtracker/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.spendingtracker
 
+import android.app.Notification
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,25 +13,31 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                loadFragment(HomeFragment.newInstance())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+                loadFragment(DashboardFragment.newInstance())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                loadFragment(NotificationFragment.newInstance())
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
-
+    private fun loadFragment(fragment:Fragment){
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadFragment(HomeFragment.newInstance())
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
