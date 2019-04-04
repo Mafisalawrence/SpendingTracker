11a3ad22468fcc600ade9937b7e28ad0e6b79fd8
lawrence <lmafisa@retrorabbit.co.za>
2019-04-04 14:02:21 +0200

    MessageReceiver

---
+++ app/src/main/java/com/example/spendingtracker/MessageReceiver.kt
@@ -0,0 +1,19 @@
+package com.example.spendingtracker
+
+import android.app.AlertDialog
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MessageReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Toast.makeText(context, intent.action, Toast.LENGTH_LONG)
+
+        if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
+            val alert = AlertDialog.Builder(context)
+            alert.setMessage("Sms receiver is working").create().show()
+        }
+    }
+}
